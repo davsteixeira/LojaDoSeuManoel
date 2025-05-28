c6a1de781cd4dd6467306e8ce0e9c6750fc932ac
Davs <123772756+davsteixeira@users.noreply.github.com>
2025-05-28 13:23:02 -0300

    feat: adicionada entidade Produto

---
+++ LojaDoSeuManoel/Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Embalagem.API.Models;
+
+public class Produto
+{
+    public string Id { get; set; }
+    public string Nome { get; set; }
+    public int Altura { get; set; }
+    public int Largura { get; set; }
+    public int Comprimento { get; set; }
+}
