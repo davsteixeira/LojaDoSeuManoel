cf2a0090d334b0ec4e19ba4248e74888aa009909
Davs <123772756+davsteixeira@users.noreply.github.com>
2025-05-28 14:33:34 -0300

    feat: adiciona dtos para entrada e saída

---
+++ LojaDoSeuManoel/Dtos/PedidoRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Embalagem.API.Dtos;
+
+public class PedidoRequest
+{
+    public int Id { get; set; }
+    public List<ProdutoDto> Produtos { get; set; } = new();
+}
+
---
+++ LojaDoSeuManoel/Dtos/ProdutoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Embalagem.API.Dtos;
+
+public class ProdutoDto
+{
+    public string Nome { get; set; } = string.Empty;
+    public decimal Altura { get; set; }
+    public decimal Largura { get; set; }
+    public decimal Comprimento { get; set; }
+}
+
