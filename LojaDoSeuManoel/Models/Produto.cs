e967bd37670b2f9d328df18c0ce22b221e1ce3b8
Davs <123772756+davsteixeira@users.noreply.github.com>
2025-05-28 16:27:03 -0300

    chore: migrations iniciais

--- LojaDoSeuManoel/Models/Produto.cs
+++ LojaDoSeuManoel/Models/Produto.cs
@@ -2,6 +2,7 @@
 
 public class Produto
 {
+    public int Id { get; set; }
     public string Nome { get; set; } = string.Empty;
     public decimal Altura { get; set; }
     public decimal Largura { get; set; }
